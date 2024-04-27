9642da2e9eeb07a7b12c51b1bf1be8b5517d6a1b
Mohil Sarvankar <mohilsarvankar23@gmail.com>
2024-04-27 21:18:27 +0530

    Navbar done

---
+++ frontend/src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from '../../components/MainCarousel/MainCarousel'
+import ProductSection from '../../components/ProductSection/ProductSection'
+
+const HomePage = () => {
+  return (
+    <>
+        <MainCarousel/>
+        <div>
+            <ProductSection/>
+        </div>
+    </>
+  )
+}
+
+export default HomePage
