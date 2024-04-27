515041dd61d97d6ecfa23e90f58488a569d26504
Mohil Sarvankar <mohilsarvankar23@gmail.com>
2024-04-27 21:19:26 +0530

    Carousel done

---
+++ frontend/src/customer/components/MainCarousel/MainCarousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import banner1 from '../../../images/banner/banner1.jpg';
+import banner2 from '../../../images/banner/banner2.jpg';
+import banner3 from '../../../images/banner/banner3.jpg';
+import banner4 from '../../../images/banner/banner4.jpg';
+
+const MainCarousel = () => {
+
+    const items = [
+        <img src={banner1} alt='banner1' className='cursor-pointer'/>,
+        <img src={banner2} alt='banner2' className='cursor-pointer'/>,
+        <img src={banner3} alt='banner3' className='cursor-pointer'/>,
+        <img src={banner4} alt='banner4' className='cursor-pointer'/>
+    ];
+
+    return (
+        <>
+            <AliceCarousel
+                items={items}
+                infinite
+                disableButtonsControls
+                autoPlay
+                autoPlayInterval={2000}
+            />
+        </>
+    )
+}
+
+export default MainCarousel
