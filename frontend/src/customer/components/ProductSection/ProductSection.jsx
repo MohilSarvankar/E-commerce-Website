13c15cbcfa82626d11f69edfa141ef79b6d32c52
Mohil Sarvankar <mohilsarvankar23@gmail.com>
2024-05-01 13:12:15 +0530

    Product section previous and next buttons functionality implemented

--- frontend/src/customer/components/ProductSection/ProductSection.jsx
+++ frontend/src/customer/components/ProductSection/ProductSection.jsx
@@ -1,42 +1,52 @@
-import React from 'react'
-import ProductCard from '../ProductCard/ProductCard'
+import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import ProductCard from '../ProductCard/ProductCard';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { Button } from '@mui/material';
+import { Button } from '@mui/material'; 
+
+const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 6 },
+};
 
 const ProductSection = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [items] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
+        <ProductCard key={index}/>
+    )));
 
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 3 },
-        1024: { items: 6 },
-    };
+    const slidePrev = () => setActiveIndex(activeIndex - 1);
+    const slideNext = () => setActiveIndex(activeIndex + 1);
+    const onSlideChanged = (e) => {
+        setActiveIndex(e.item);
+        console.log(activeIndex);
+    }
 
-    const items = [1, 1, 1, 1, 1, 1, 1].map((item) => {
-        return <ProductCard />
-    })
-
-    return (
+    return [
         <div className='px-5 lg:px-8'>
             <div className='relative p-5'>
                 <AliceCarousel
-                    items={items}
-                    infinite
-                    disableButtonsControls
+                    mouseTracking
                     disableDotsControls
+                    disableButtonsControls
+                    items={items}
+                    activeIndex={activeIndex}
                     responsive={responsive}
+                    onSlideChanged={onSlideChanged}
                 />
-                <Button variant='contained' sx={{position:'absolute', top: '45%', left: '-2rem', transform:'rotate(90deg)', bgcolor:'white', color:'black',zIndex:'50', "&:hover":{bgcolor:'whitesmoke'}}} aria-label='prevous' >
+                {activeIndex != 0 && <Button onClick={slidePrev} variant='contained' sx={{position:'absolute', top: '45%', left: '-2rem', transform:'rotate(90deg)', bgcolor:'white', color:'black',zIndex:'50', "&:hover":{bgcolor:'whitesmoke'}}} aria-label='previous' >
                     <KeyboardArrowLeftIcon className='-rotate-90'/> 
-                </Button>
-                <Button variant='contained' sx={{position:'absolute', top: '45%', right: '-2rem', transform:'rotate(90deg)', bgcolor:'white', color:'black',zIndex:'50', "&:hover":{bgcolor:'whitesmoke'}}} aria-label='next'>
+                </Button>}
+                {/* I have hardcoded 6 here. But i need to change it and make it dynamic */}
+                {activeIndex != items.length - 6 && <Button onClick={slideNext} variant='contained' sx={{position:'absolute', top: '45%', right: '-2rem', transform:'rotate(90deg)', bgcolor:'white', color:'black',zIndex:'50', "&:hover":{bgcolor:'whitesmoke'}}} aria-label='next'>
                     <KeyboardArrowRightIcon className='-rotate-90'/>
-                </Button>
+                </Button>}
             </div>
         </div>
-    )
-}
+    ];
+};
 
-export default ProductSection
+export default ProductSection;
